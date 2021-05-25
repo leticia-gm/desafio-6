0e6f270545a82f11606823df0a5aa56fd88b7dfc
leticia-gm <leticiagmagretti@gmail.com>
2021-05-25 11:08:52 -0300

    feito

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const trasactionRepository = getCustomRepository(TransactionsRepository);
+    const transactions = await trasactionRepository.findOne(id);
+    if (!transactions) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    const response = await trasactionRepository.remove(transactions);
   }
 }
 
